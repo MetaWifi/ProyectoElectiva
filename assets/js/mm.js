90093eabe091367830ee2ac317b0485c515a3d17
hubercorrea98 <88495735+hubercorrea98@users.noreply.github.com>
2024-11-18 13:56:39 -0500

    Create mm.js

---
+++ assets/js/mm.js
@@ -0,0 +1,5 @@
+ document.getElementById('invoice-display').innerHTML = `
+                <p><strong>Cliente:</strong> ${cliente}</p>
+                <p><strong>Servicio:</strong> ${servicio}</p>
+                <p><strong>Monto:</strong> $${monto}</p>
+            `;
