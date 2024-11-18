96e38419b0846b45bf42d01c0b5b4df61190f217
hubercorrea98 <88495735+hubercorrea98@users.noreply.github.com>
2024-11-18 13:57:28 -0500

    Delete assets/js/mm.js

--- assets/js/mm.js
+++
@@ -1,5 +0,0 @@
- document.getElementById('invoice-display').innerHTML = `
-                <p><strong>Cliente:</strong> ${cliente}</p>
-                <p><strong>Servicio:</strong> ${servicio}</p>
-                <p><strong>Monto:</strong> $${monto}</p>
-            `;
